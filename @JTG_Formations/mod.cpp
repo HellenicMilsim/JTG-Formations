9c4ace2e8641bc2cdd93631a2d55aa9ea9cb3aad
Tony Klinakis <a.klinakis@yahoo.com>
2017-09-02 03:27:03 +0100

    Initial commit

---
+++ @JTG_Formations/mod.cpp
@@ -0,0 +1,17 @@
+name = "JTG Formations";
+description = "Group definitions for JTG_Formations";
+picture = "logo256.paa";
+hidePicture = "false";
+hideName = "false";
+actionName = "Website";
+action = "https://hellenic-milsim.community";
+version = "1.2";
+
+logo = "logo_128_ca.paa";
+logoOver = "logo_256_ca.paa";
+logoSmall = "logo_32_ca.paa";
+
+tooltip = "JimTheGreek's Factions Formations";
+tooltipOwned = "JimTheGreek's Factions - Owned";
+overview = "Equipment of several armies around the globe, including a variety of uniforms, vests, backpacks and headgear.<br/><br/>https://hellenic-milsim.community";
+author = "JimTheGreek";

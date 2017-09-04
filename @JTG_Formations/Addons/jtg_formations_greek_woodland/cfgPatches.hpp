fe7045ddb5ad3c31351063cd90556135c0384531
Tony Klinakis <a.klinakis@yahoo.com>
2017-09-05 00:59:32 +0100

    Rename faction to Greek Army (Woodland)

--- @JTG_Formations/Addons/jtg_formations_greek_woodland/cfgPatches.hpp
+++ @JTG_Formations/Addons/jtg_formations_greek_woodland/cfgPatches.hpp
@@ -1,6 +1,8 @@
 class CfgPatches {
     class JTG_GR {
         units[] = {
+            "jtg_gr_infantry_rifleman",
+            "jtg_grd_infantry_rifleman",
             "B_JTGGR_Rifleman",
             "B_JTGGR_Rifleman_AT",
             "B_JTGGR_Machinegunner",
@@ -9,19 +11,13 @@ class CfgPatches {
             "B_JTGGR_Team_Leader",
             "B_JTGGR_Rifleman_GL",
             "B_JTGGR_Pilot",
-            "B_JTGGR_AH_1Z_CAS",
-            "B_JTGGR_M113_M2",
-            "B_JTGGR_Crewman",
-            "B_JTGGR_HMMWV_M2"
+            "B_JTGGR_Crewman"
         };
         weapons[] = {};
         requiredVersion = 1.62;
         requiredAddons[] = {
             "rhssaf_c_troops",
-            "RHS_US_A2_AirImport",
-            "rhsusf_c_m113",
-            "ace_compat_rhs_usf3",
-            "rhsusf_vehicles",
+            "jtg_faction_greece",
             "A3_Modules_F",
             "A3_Modules_F_Curator"
         };

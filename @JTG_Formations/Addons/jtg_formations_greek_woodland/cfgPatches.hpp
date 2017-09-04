f610dada4261374ed81a1e142dee7acade87c7d1
Tony Klinakis <a.klinakis@yahoo.com>
2017-09-05 00:54:43 +0100

    Break cfgPatches to a different file, remove some units
    Removed some units to re-do them

---
+++ @JTG_Formations/Addons/jtg_formations_greek_woodland/cfgPatches.hpp
@@ -0,0 +1,31 @@
+class CfgPatches {
+    class JTG_GR {
+        units[] = {
+            "B_JTGGR_Rifleman",
+            "B_JTGGR_Rifleman_AT",
+            "B_JTGGR_Machinegunner",
+            "B_JTGGR_Machinegunner_Asst",
+            "B_JTGGR_Rifleman_SAW",
+            "B_JTGGR_Team_Leader",
+            "B_JTGGR_Rifleman_GL",
+            "B_JTGGR_Pilot",
+            "B_JTGGR_AH_1Z_CAS",
+            "B_JTGGR_M113_M2",
+            "B_JTGGR_Crewman",
+            "B_JTGGR_HMMWV_M2"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "rhssaf_c_troops",
+            "RHS_US_A2_AirImport",
+            "rhsusf_c_m113",
+            "ace_compat_rhs_usf3",
+            "rhsusf_vehicles",
+            "A3_Modules_F",
+            "A3_Modules_F_Curator"
+        };
+        author = "+= rath";
+        authors[] = { "+= rath" };
+    };
+};

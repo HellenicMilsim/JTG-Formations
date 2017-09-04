31f5fd2fbfd0ba022b266243347732017fd85c10
Tony Klinakis <a.klinakis@yahoo.com>
2017-09-05 00:54:21 +0100

    Break cfgPatches to a different file

---
+++ @JTG_Formations/Addons/jtg_formations_turkish/cfgPatches.hpp
@@ -0,0 +1,23 @@
+class CfgPatches {
+    class JTG_TRK {
+        units[] = {
+            "B_JTGTRK_Rifleman",
+            "B_JTGTRK_Rifleman_AT",
+            "B_JTGTRK_Machinegunner",
+            "B_JTGTRK_MG_Assistant",
+            "B_JTGTRK_Team_Leader",
+            "B_JTGTRK_Rifleman_GL",
+            "B_JTGTRK_Rifleman_SAW"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "rhs_c_troops",
+            "A3_Modules_F",
+            "A3_Modules_F_Curator"
+        };
+        author = "+= rath";
+        authors[] = { "+= rath" };
+    };
+};
+
--- @JTG_Formations/Addons/jtg_formations_turkish/config.cpp
+++ @JTG_Formations/Addons/jtg_formations_turkish/config.cpp
@@ -1,25 +1,4 @@
-class CfgPatches {
-    class JTG_TRK {
-        units[] = {
-            "B_JTGTRK_Rifleman",
-            "B_JTGTRK_Rifleman_AT",
-            "B_JTGTRK_Machinegunner",
-            "B_JTGTRK_MG_Assistant",
-            "B_JTGTRK_Team_Leader",
-            "B_JTGTRK_Rifleman_GL",
-            "B_JTGTRK_Rifleman_SAW"
-        };
-        weapons[] = {};
-        requiredVersion = 1.62;
-        requiredAddons[] = {
-            "rhs_c_troops"
-        };
-        author = "+= rath";
-        authors[] = { "+= rath" };
-    };
-};
-
-
+#include "cfgPatches.hpp"
 
 
 //////////////////////////////////////////////////////////////////////////////////

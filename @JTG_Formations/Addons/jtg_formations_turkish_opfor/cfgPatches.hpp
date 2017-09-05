e3d80e4ba670528fe6083811bafa8c810ebc53f0
Tony Klinakis <a.klinakis@yahoo.com>
2017-09-05 04:00:45 +0100

    Add TR OPFOR faction
    Copy of TR with different faction prefixes

---
+++ @JTG_Formations/Addons/jtg_formations_turkish_opfor/cfgPatches.hpp
@@ -0,0 +1,25 @@
+class CfgPatches {
+    class JTG_TRK_OPF {
+        units[] = {
+            "jtg_trk_infantry_rifleman",
+            "B_JTGTRK_OPF_Rifleman_SAW",
+            "B_JTGTRK_OPF_Rifleman_AT",
+            "B_JTGTRK_OPF_MG_Assistant",
+            "B_JTGTRK_OPF_Team_Leader",
+            "B_JTGTRK_OPF_Rifleman_GL",
+            "B_JTGTRK_OPF_Machinegunner",
+            "B_JTGTRK_OPF_Officer",
+            "B_JTGTRK_OPF_2IC",
+            "B_JTGTRK_OPF_FAC"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "rhs_c_troops",
+            "A3_Modules_F",
+            "A3_Modules_F_Curator"
+        };
+        author = "+= rath";
+        authors[] = { "+= rath" };
+    };
+};
